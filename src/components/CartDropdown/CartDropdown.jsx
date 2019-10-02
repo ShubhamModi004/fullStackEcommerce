6142741aee6b70a87edc6d35b3b45b870962745a
shubhammodi004 <shubhammodi004@example.com>
2019-10-02 22:40:17 +0530

    updating our application reselect with memoize selectors

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux';
 
 import './CartDropdown.styles.scss';
 import CustomButton from '../CustomButton/CustomButton';
+import CartItem from '../CartItem/CartItem';
+import { selectCartItems } from '../../redux/cart/cart-selectors';
 
-const CartDropdown = props => {
+const CartDropdown = ({ cartItems }) => {
     return (
         <div className='cart-dropdown'>
             <div className='cart-items' />
+            {cartItems &&
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+            }
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     )
 }
 
-CartDropdown.propTypes = {
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
+})
 
-}
-
-export default CartDropdown
+export default connect(mapStateToProps)(CartDropdown);
